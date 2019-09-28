9a05898faf889c1cc3dcebb6f4c890eced6db172
yyksir <1360032149@qq.com>
2019-09-28 20:57:11 +0800

    1

--- pages/study/dialect/index.vue
+++ pages/study/dialect/index.vue
@@ -159,9 +159,12 @@ export default {
                 console.log(res)
                 // const resdata = res.data
                 //window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
-
+                    let audioDomEn = this.$refs.audioDomEn;
+                        audioDomEn.crossOrigin = "anonymous";
+                        audioDomEn.src = _that.urlVoice + res.data.wordName + 0 + '.mp3'; 
+                        audioDomEn.play()
                    
-                        _that.handleBtnVoiceEnClick()
+                        // _that.handleBtnVoiceEnClick()
                        // audioDomEn.play()
                 //     let MEDIA_ELEMENT_NODES = new WeakMap();
                 //     if (ctx == undefined) { 
